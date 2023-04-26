603accfa8f75b1e52017a0c97719a07cede06075
Georgy Usatenko <g.usatenko@tinkoff.ru>
2023-04-26 15:11:09 +0300

    add helm files

--- application/app.py
+++ application/app.py
@@ -5,7 +5,12 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+
+redis_host = os.environ['DATABASE_URL']
+redis_password = os.environ['DATABASE_PASSWORD']
+redis_port = int(os.environ['DATABASE_PORT'])
+
+cache = redis.Redis(host=redis_host, password=redis_password, port=redis_port)
 
 
 def get_hit_count():
@@ -24,4 +29,3 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
-
